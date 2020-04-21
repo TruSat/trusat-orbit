17a4a79985ad16499068e1cc193fc3e65d357e1a
Chris Lewicki <chris@lewicki.com>
2020-04-21 12:14:42 -0700

    Cleaning up deleted files

--- tuning_test_setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules=cythonize(["tuning_test.pyx"]),
-	include_dirs=[numpy.get_include()]
-)
-
